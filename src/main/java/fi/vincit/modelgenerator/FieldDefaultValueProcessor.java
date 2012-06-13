a54fa77bab53bcd970438c69125950f398940327
Juha Siponen <juha.siponen@vincit.fi>
2012-06-13 20:37:59 +0300

    Added new test for default values
    Add FieldDefaultValueProcessorTest to test default value formatting
    Remove default string casting from map and array because it won't work

--- src/main/java/fi/vincit/modelgenerator/FieldDefaultValueProcessor.java
+++ src/main/java/fi/vincit/modelgenerator/FieldDefaultValueProcessor.java
@@ -15,20 +15,20 @@ public class FieldDefaultValueProcessor {
             .getLogger( FieldDefaultValueProcessor.class );
 
     public String getDefaultValue(Field field, Object defaultObject) {
-        Class<?> t = field.getType();
+        Class<?> fieldType = field.getType();
         String defaultValue = extractDefaultValue( field, defaultObject );
-        if( t.equals( String.class ) ) {
+        if( fieldType.equals( String.class ) ) {
             return defaultValue != null ? "\""+defaultValue+"\"" : "\"\"";
-        } else if( t.equals(Integer.class) || t.equals(Long.class) || t.equals(int.class) || t.equals(long.class) ) {
+        } else if( fieldType.equals(Integer.class) || fieldType.equals(Long.class) || fieldType.equals(int.class) || fieldType.equals(long.class) ) {
             return defaultValue != null ? defaultValue : "0";
-        } else if( t.equals(Float.class) || t.equals(Double.class) || t.equals(float.class) || t.equals(double.class) ) {
+        } else if( fieldType.equals(Float.class) || fieldType.equals(Double.class) || fieldType.equals(float.class) || fieldType.equals(double.class) ) {
             return defaultValue != null ? defaultValue : "0.0";
-        } else if( t.equals(Array.class) || t.isArray() || t.equals(List.class) || t.equals(Collection.class) ) {
-            return defaultValue != null ? defaultValue : "[]";
-        } else if( t.equals(Boolean.class) || t.equals(boolean.class) ) {
+        } else if( fieldType.equals(Array.class) || fieldType.isArray() || fieldType.equals(List.class) || fieldType.equals(Collection.class) ) {
+            return "[]";
+        } else if( fieldType.equals(Boolean.class) || fieldType.equals(boolean.class) ) {
             return defaultValue != null ? defaultValue : "false";
-        } else if( t.equals(Map.class) || t.equals(Set.class) ) {
-            return defaultValue != null ? defaultValue : "{}";
+        } else if( fieldType.equals(Map.class) || fieldType.equals(Set.class) ) {
+            return "{}";
         }
 
         LOG.error("Invalid type of class {} for field {}", field.getType().toString(), field.getName());
@@ -38,9 +38,11 @@ public class FieldDefaultValueProcessor {
     private String extractDefaultValue( Field field, Object defaultObject ) {
         String defaultValue;
         try {
+            field.setAccessible(true);
             Object fieldValue = field.get( defaultObject );
             defaultValue = fieldValue != null ? "" + fieldValue : null;
         } catch( IllegalAccessException e ) {
+            LOG.error("Could not access field {}", field.getName());
             defaultValue = null;
         }
         return defaultValue;
