e19cfdb64e3982d8d0a3d8a60f8a376cdeb1fced
Juha Siponen <juha.siponen@vincit.fi>
2012-06-23 17:32:52 +0300

    Add new exception and more documentation

--- src/main/java/fi/vincit/jmobster/JMobsterFactory.java
+++ src/main/java/fi/vincit/jmobster/JMobsterFactory.java
@@ -1,5 +1,6 @@
 package fi.vincit.jmobster;
 
+import fi.vincit.jmobster.exception.UnsupportedFramework;
 import fi.vincit.jmobster.processor.AnnotationProcessorProvider;
 import fi.vincit.jmobster.processor.frameworks.backbone.DefaultAnnotationProcessorProvider;
 import fi.vincit.jmobster.processor.languages.javascript.JavaToJSValueConverter;
@@ -9,7 +10,24 @@ import fi.vincit.jmobster.processor.frameworks.backbone.BackboneModelProcessor;
 import fi.vincit.jmobster.processor.ModelProcessor;
 import fi.vincit.jmobster.util.ModelWriter;
 
+/**
+ * <p>
+ *     JMobsterFactory creates model generators for different frameworks. The factory
+ *     configures the generators with appropriate processors by default.
+ * </p>
+ * <p>
+ *     Currently the only supported framework is Backbone.js. The framework ID for it is
+ *     "backbone.js" (also "backbone" works). The IDs are case insensitive.
+ * </p>
+ */
 public class JMobsterFactory {
+    /**
+     * Creates a model generator instance for the given framework.
+     * @param framework Framework ID
+     * @param writer Writer to use
+     * @return Configured model generator
+     * @throws UnsupportedFramework If the framework is not supported
+     */
     public static ModelGenerator getInstance(String framework, ModelWriter writer) {
         if( "backbone.js".equalsIgnoreCase(framework) || "backbone".equalsIgnoreCase(framework) ) {
             ModelProcessor modelProcessor = new BackboneModelProcessor(writer);
@@ -19,7 +37,7 @@ public class JMobsterFactory {
                     new JavaToJSValueConverter(ConverterMode.NULL_AS_DEFAULT),
                     annotationProcessorProvider);
         } else {
-            throw new IllegalArgumentException("Framework " + framework + "not supported");
+            throw new UnsupportedFramework("Framework " + framework + " not supported");
         }
     }
 }
