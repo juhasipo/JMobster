11677a7cb2178e735528c9b3ac9e1feea110e681
Juha Siponen <juha.siponen@vincit.fi>
2013-03-08 19:33:36 +0200

    Add tests for DefaultValueProcessor

---
+++ src/test/java/fi/vincit/jmobster/processor/frameworks/backbone/DefaultValueProcessorTest.java
@@ -0,0 +1,197 @@
+package fi.vincit.jmobster.processor.frameworks.backbone;
+
+/*
+ * Copyright 2012 Juha Siponen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import fi.vincit.jmobster.processor.FieldValueConverter;
+import fi.vincit.jmobster.processor.model.Model;
+import fi.vincit.jmobster.processor.model.ModelField;
+import fi.vincit.jmobster.util.itemprocessor.ItemStatuses;
+import fi.vincit.jmobster.util.writer.StringBufferWriter;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class DefaultValueProcessorTest {
+
+    private StringBufferWriter writer;
+    @Mock private FieldValueConverter fieldValueConverter;
+
+    @Before
+    public void init() {
+        writer = new StringBufferWriter();
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testStartProcessingFirst() throws Exception {
+        DefaultValueProcessor processor = createProcessor();
+
+        processor.startProcessing(ItemStatuses.first());
+
+        assertThat(writer.toString(), is("function() {\n"));
+    }
+
+    @Test
+    public void testStartProcessingMiddle() throws Exception {
+        DefaultValueProcessor processor = createProcessor();
+
+        processor.startProcessing(ItemStatuses.notFirstNorLast());
+
+        assertThat(writer.toString(), is("function() {\n"));
+    }
+
+    @Test
+    public void testStartProcessingLast() throws Exception {
+        DefaultValueProcessor processor = createProcessor();
+
+        processor.startProcessing(ItemStatuses.last());
+
+        assertThat(writer.toString(), is("function() {\n"));
+    }
+
+    @Test
+    public void testEndProcessingFirst() throws Exception {
+        DefaultValueProcessor processor = createProcessor();
+
+        processor.endProcessing(ItemStatuses.first());
+
+        assertThat(writer.toString(), is("},\n"));
+    }
+
+    @Test
+    public void testEndProcessingMiddle() throws Exception {
+        DefaultValueProcessor processor = createProcessor();
+
+        processor.endProcessing(ItemStatuses.notFirstNorLast());
+
+        assertThat(writer.toString(), is("},\n"));
+    }
+
+    @Test
+    public void testEndProcessingLast() throws Exception {
+        DefaultValueProcessor processor = createProcessor();
+
+        processor.endProcessing(ItemStatuses.last());
+
+        assertThat(writer.toString(), is("}\n"));
+    }
+
+    @Test
+    public void testProcessFirstModel() {
+        DefaultValueProcessor processor = createProcessor();
+
+        Model model = mock(Model.class);
+        ModelField field1 = mock(ModelField.class); {
+            when(field1.getFieldType()).thenReturn(String.class);
+            when(field1.getName()).thenReturn("field1");
+        }
+
+        List<ModelField> fields = new ArrayList<ModelField>();
+        fields.add(field1);
+        when(model.getFields()).thenReturn(fields);
+        mockFieldValueConverter();
+
+        processor.processModel(model, ItemStatuses.first());
+
+        assertThat(writer.toString(), is(
+                "return {\n" +
+                "    field1: 'Text'\n" +
+                "}\n"
+        ));
+    }
+
+    @Test
+    public void testProcessFirstModelMultipleFields() {
+        DefaultValueProcessor processor = createProcessor();
+
+        Model model = mock(Model.class);
+
+        ModelField field1 = mock(ModelField.class); {
+            when(field1.getFieldType()).thenReturn(String.class);
+            when(field1.getName()).thenReturn("field1");
+        }
+        ModelField field2 = mock(ModelField.class); {
+            when(field2.getFieldType()).thenReturn(Long.class);
+            when(field2.getName()).thenReturn("field2");
+        }
+
+        List<ModelField> fields = new ArrayList<ModelField>();
+        fields.add(field1);
+        fields.add(field2);
+        when(model.getFields()).thenReturn(fields);
+        mockFieldValueConverter();
+
+        processor.processModel(model, ItemStatuses.first());
+
+        assertThat(writer.toString(), is(
+                "return {\n" +
+                "    field1: 'Text',\n" +
+                "    field2: 'Number'\n" +
+                "}\n"
+        ));
+    }
+
+    @Test
+    public void testProcessLastModelMultipleFields() {
+        DefaultValueProcessor processor = createProcessor();
+
+        Model model = mock(Model.class);
+
+        ModelField field1 = mock(ModelField.class); {
+            when(field1.getFieldType()).thenReturn(String.class);
+            when(field1.getName()).thenReturn("field1");
+        }
+        ModelField field2 = mock(ModelField.class); {
+            when(field2.getFieldType()).thenReturn(Long.class);
+            when(field2.getName()).thenReturn("field2");
+        }
+
+        List<ModelField> fields = new ArrayList<ModelField>();
+        fields.add(field1);
+        fields.add(field2);
+        when(model.getFields()).thenReturn(fields);
+        mockFieldValueConverter();
+
+        processor.processModel(model, ItemStatuses.last());
+
+        assertThat(writer.toString(), is(
+                "return {\n" +
+                        "    field1: 'Text',\n" +
+                        "    field2: 'Number'\n" +
+                        "}\n"
+        ));
+    }
+
+    private void mockFieldValueConverter() {
+        when(fieldValueConverter.convert(String.class, null)).thenReturn("'Text'");
+        when(fieldValueConverter.convert(Long.class, null)).thenReturn("'Number'");
+    }
+
+    private DefaultValueProcessor createProcessor() {
+        return new DefaultValueProcessor(writer, fieldValueConverter);
+    }
+
+
+}
---
+++ src/test/java/fi/vincit/jmobster/processor/frameworks/backbone/MockWriter.java
@@ -0,0 +1,99 @@
+package fi.vincit.jmobster.processor.frameworks.backbone;
+
+/*
+ * Copyright 2012 Juha Siponen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import fi.vincit.jmobster.util.writer.DataWriter;
+
+class MockWriter implements DataWriter {
+
+    private DataWriter mockWriter;
+
+    MockWriter(DataWriter mockWriter) {
+        this.mockWriter = mockWriter;
+    }
+
+    @Override
+    public boolean isOpen() {
+        return mockWriter.isOpen();
+    }
+
+    @Override
+    public DataWriter write(char c) {
+        mockWriter.write("" + c);
+        return this;
+    }
+
+    @Override
+    public DataWriter write(String modelString) {
+        mockWriter.write(modelString);
+        return this;
+    }
+
+    @Override
+    public DataWriter write(String modelString, String separator, boolean writeSeparator) {
+        if( writeSeparator ) {
+            mockWriter.write(modelString + separator);
+        } else {
+            mockWriter.write(modelString);
+        }
+        return this;
+    }
+
+    @Override
+    public DataWriter writeLine(String modelStringLine) {
+        mockWriter.write(modelStringLine + "\n");
+        return this;
+    }
+
+    @Override
+    public DataWriter writeLine(String modelStringLine, String separator, boolean writeSeparator) {
+
+        if( writeSeparator ) {
+            mockWriter.write(modelStringLine + separator + "\n");
+        } else {
+            mockWriter.write(modelStringLine + "\n");
+        }
+        return this;
+    }
+
+    @Override
+    public void close() {
+        mockWriter.close();
+    }
+
+    @Override
+    public void setIndentation(int spaces) {
+    }
+
+    @Override
+    public DataWriter indent() {
+        return this;
+    }
+
+    @Override
+    public DataWriter indentBack() {
+        return this;
+    }
+
+    @Override
+    public void setIndentationChar(char indentationChar, int characterCount) {
+    }
+
+    @Override
+    public void setLineSeparator(String lineSeparator) {
+    }
+}
