8e1df42c4332b098e767051bee9857bd4ee08f86
Juha Siponen <juha.siponen@vincit.fi>
2013-04-18 22:33:29 +0300

    Add HTML5Writer
    Added a basic HTML5Writer for generating forms. The current implementation
    doesn't yet support many kind of tags or tag types (e.g. empty XML tag).
    Those will be added later.

---
+++ src/main/java/fi/vincit/jmobster/processor/languages/html5/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * HTML5 specific implementations of utility classes and converters
+ */
+package fi.vincit.jmobster.processor.languages.html5;
---
+++ src/main/java/fi/vincit/jmobster/processor/languages/html5/writer/HTML5Writer.java
@@ -0,0 +1,12 @@
+package fi.vincit.jmobster.processor.languages.html5.writer;
+
+import fi.vincit.jmobster.processor.languages.BaseDataWriter;
+import fi.vincit.jmobster.util.writer.DataWriter;
+
+public class HTML5Writer extends BaseDataWriter<HTML5Writer> {
+
+    public HTML5Writer(DataWriter writer) {
+        super(writer);
+    }
+
+}
