f79d2a910f4bfb54d6672b6e514dc78d1989e471
Juha Siponen <juha.siponen@vincit.fi>
2013-04-20 10:14:27 +0300

    Add LanguageContext

---
+++ src/main/java/fi/vincit/jmobster/processor/languages/LanguageContext.java
@@ -0,0 +1,31 @@
+package fi.vincit.jmobster.processor.languages;
+
+/*
+ * Copyright 2012 Juha Siponen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import fi.vincit.jmobster.util.writer.DataWriter;
+
+public class LanguageContext<W extends DataWriter> {
+    private W writer;
+
+    public LanguageContext(W writer) {
+        this.writer = writer;
+    }
+
+    public W getWriter() {
+        return writer;
+    }
+}
