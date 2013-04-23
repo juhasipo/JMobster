65b7554c4c14f3392c8a6a29d0f35b63345fcf1f
Juha Siponen <juha.siponen@vincit.fi>
2013-04-23 21:50:59 +0300

    Add LengthValidator test for HibernateValidatorFactory

---
+++ src/main/java/fi/vincit/jmobster/processor/defaults/validator/hibernate/HibernateValidatorFactory.java
@@ -0,0 +1,31 @@
+package fi.vincit.jmobster.processor.defaults.validator.hibernate;
+
+/*
+ * Copyright 2012 Juha Siponen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import fi.vincit.jmobster.processor.defaults.validator.BaseValidatorFactory;
+import fi.vincit.jmobster.util.combination.OptionalTypes;
+import fi.vincit.jmobster.util.combination.RequiredTypes;
+import org.hibernate.validator.constraints.Length;
+
+public class HibernateValidatorFactory extends BaseValidatorFactory {
+    public HibernateValidatorFactory() {
+        setValidator(
+                LengthValidator.class,
+                RequiredTypes.get(Length.class),
+                OptionalTypes.get());
+    }
+}
